07871f643022fae123565d9da871080533ad21d3
AbuBokorprog <abubokor1066@gmail.com>
2024-10-13 10:01:04 +0600

    improve layout

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -1,31 +1,29 @@
-import { Inter } from "next/font/google";
-import "../globals.css";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
-import Provider from "@/provider/contextProvider";
-import Home from "@/components/home";
-import { ThemeProvider } from "next-themes";
-import Cursor from "@/components/cursor/cursor";
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
+import Provider from '@/provider/contextProvider';
+import Home from '@/components/home';
+import { ThemeProvider } from 'next-themes';
+import Cursor from '@/components/cursor/cursor';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Abu Bokor || Front-End Developer",
-  description: "Frontend Developer Portfolio",
+  title: 'Abu Bokor || Front-End Developer',
+  description: 'Frontend Developer Portfolio',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} container mx-auto bg-white dark:bg-black `}
-      >
+      <body className={`${inter.className}bg-white dark:bg-black `}>
         <Home>
           <ThemeProvider>
             <Provider>
               <Cursor />
               <Navbar />
-              {children}
+              <div className="container mx-auto">{children}</div>
               <Footer />
             </Provider>
           </ThemeProvider>
