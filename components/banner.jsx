d2c6796da9f2d22aeea8d5f9c7c9ff6688122d9d
AbuBokorprog <abubokor1066@gmail.com>
2024-10-14 22:27:11 +0600

    Background beams added.

--- components/banner.jsx
+++ components/banner.jsx
@@ -5,6 +5,7 @@ import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import { FaGithub, FaInstagramSquare, FaLinkedin } from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
+import { BackgroundBeamsWithCollision } from './ui/background-beams-with-collision';
 gsap.registerPlugin(ScrollTrigger);
 
 const Banner = () => {
@@ -38,47 +39,51 @@ const Banner = () => {
   }, []);
 
   return (
-    <section
-      ref={container}
-      id="#"
-      className="relative lg:my-10 min-h-screen flex items-center justify-center dark:text-white"
-    >
-      <div>
-        <h1 className="text-6xl md:text-8xl lg:text-9xl font-extrabold text-center bg-gradient-to-bl dark:from-fuchsia-800 dark:to-rose-800 from-fuchsia-500 to-rose-500 bg-clip-text text-transparent">
-          <span className="lg:pe-20 title">Hi there, I'm</span> <br />
-          <span className="lg:ps-20 title">Abu Bokor.</span>
-        </h1>
-        {/* Typing Animation for roles */}
-        <div className="mt-5 title text-center text-2xl md:text-4xl font-medium text-white">
-          <TypeAnimation
-            sequence={[
-              'Frontend Developer',
-              2000,
-              'MERN Stack Developer',
-              2000,
-              'Full Stack Developer',
-              2000,
-            ]}
-            repeat={Infinity}
-            speed={40}
-          />
-        </div>
-        <div className="flex title justify-center items-center gap-4 md:gap-8 mt-8">
-          <a href="https://github.com/AbuBokorprog" className="hoverable">
-            <FaGithub className="w-8 md:w-12 h-8 md:h-12 dark:text-white text-black" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/md-abu-bokor/"
-            className="hoverable"
-          >
-            <FaLinkedin className="w-8 md:w-12 h-8 md:h-12 dark:text-white text-black" />
-          </a>
-          <a href="#" className="hoverable">
-            <FaInstagramSquare className="w-8 md:w-12 h-8 md:h-12 dark:text-white text-black" />
-          </a>
-        </div>
-      </div>
-    </section>
+    <>
+      <BackgroundBeamsWithCollision>
+        <section
+          ref={container}
+          id="#"
+          className="relative h-96 md:h-[40rem] flex items-center justify-center dark:text-white"
+        >
+          <div>
+            <h1 className="text-6xl md:text-8xl lg:text-9xl font-extrabold text-center bg-gradient-to-bl dark:from-fuchsia-800 dark:to-rose-800 from-fuchsia-500 to-rose-500 bg-clip-text text-transparent">
+              <span className="lg:pe-20 title">Hi there, I'm</span> <br />
+              <span className="lg:ps-20 title">Abu Bokor.</span>
+            </h1>
+            {/* Typing Animation for roles */}
+            <div className="mt-5 title text-center text-2xl md:text-4xl font-medium text-white">
+              <TypeAnimation
+                sequence={[
+                  'Frontend Developer',
+                  2000,
+                  'MERN Stack Developer',
+                  2000,
+                  'Full Stack Developer',
+                  2000,
+                ]}
+                repeat={Infinity}
+                speed={40}
+              />
+            </div>
+            <div className="flex title justify-center items-center gap-4 md:gap-8 mt-8">
+              <a href="https://github.com/AbuBokorprog" className="hoverable">
+                <FaGithub className="w-8 md:w-12 h-8 md:h-12 dark:text-white text-black" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/md-abu-bokor/"
+                className="hoverable"
+              >
+                <FaLinkedin className="w-8 md:w-12 h-8 md:h-12 dark:text-white text-black" />
+              </a>
+              <a href="#" className="hoverable">
+                <FaInstagramSquare className="w-8 md:w-12 h-8 md:h-12 dark:text-white text-black" />
+              </a>
+            </div>
+          </div>
+        </section>
+      </BackgroundBeamsWithCollision>
+    </>
   );
 };
 
